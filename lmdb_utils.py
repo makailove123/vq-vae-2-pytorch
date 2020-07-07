48b2f28e122da37a0d9a8b58253e10486e029b0a
makai <makailove123@gmail.com>
2020-07-07 21:03:43 +0800

    build img lmdb

--- lmdb_utils.py
+++ lmdb_utils.py
@@ -4,6 +4,7 @@ import sys
 import os
 import lmdb
 import numpy as np
+from train_vqvae import get_key_path
 
 
 def build_lmdb_from_encode_result(args):
@@ -25,6 +26,37 @@ def build_lmdb_from_encode_result(args):
             key = arr[0]
             val = np.asarray([int(x) for x in arr[1].split(",")], dtype=np.int16)
             txn.put(key.encode("utf-8"), val.tobytes())
+    txn.commit()
+    lmdb_env.close()
+
+
+def build_img_lmdb(args):
+    img_root_path = args[0]
+    img_key_list_file = args[1]
+    output_dir = args[2]
+
+    lmdb_env = lmdb.open(output_dir, map_size=1099511627776)
+    txn = lmdb_env.begin(write=True)
+
+    linecnt = 0
+    commit_cache = 0
+    for line in open(img_key_list_file):
+        linecnt += 1
+        if linecnt % 10000 == 0:
+            print("{} done".format(linecnt), file=sys.stderr, flush=True)
+        img_key = line.strip()
+        key_enc = img_key.encode("utf-8")
+        img_path = get_key_path(img_root_path, img_key)
+        try:
+            img = open(img_path, "rb").read()
+        except:
+            continue
+        txn.put(key_enc, img)
+        commit_cache += 1
+        if commit_cache > 10000:
+            txn.commit()
+            txn = lmdb_env.begin(write=True)
+            commit_cache = 0
     txn.commit()
     lmdb_env.close()
 
